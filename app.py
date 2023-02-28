3045b47a05c0b6f155a78c439a7f4510388b19f3
KonuTech <borowiec.k@gmail.com>
2023-02-28 17:34:42 +0100

    yet another change

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+activities = ["Test activity", "Test activity 2"]
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Activity Tracker - Home")
+    return render_template("index.html", activities=activities, title="Activity Tracker - Home")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_activity():
-    return render_template("add_habit.html", title="Activity Tracker - Add Activity")
+    if request.method == "POST":
+        activity = request.form.get("activity")
+        activities.append(activity)
+    return render_template("add_activity.html", title="Activity Tracker - Add Activity")
