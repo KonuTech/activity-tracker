9d9a9e76cc3e720ae5e1a9f2dc1f34e9d738147b
KonuTech <borowiec.k@gmail.com>
2023-03-01 10:55:29 +0100

    yet another change

--- routes.py
+++ routes.py
@@ -1,60 +1,81 @@
 import datetime
-from collections import defaultdict
-from flask import Blueprint, render_template, request, redirect, url_for
+import uuid
+from flask import Blueprint, current_app, render_template, request, redirect, url_for
 
 
 pages = Blueprint("activities", __name__, template_folder="templates", static_folder="static")
-activities = ["Test activity"]
-completions = defaultdict(list)
+# activities = ["Test activity"]
+# completions = defaultdict(list)
 
 
-def date_range(start: datetime.date):
-    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-    return dates
+# def date_range(start: datetime.datetime):
+#     dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+#     return dates
 
 
 @pages.context_processor
 def add_calc_date_range():
-    def date_range(start: datetime.date):
+    def date_range(start: datetime.datetime):
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
         return dates
 
     return {"date_range": date_range}
 
 
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year, today.month, today.day)  # by default, time, min, sec are 0
+
+
 @pages.route("/")
 def index():
     date_str = request.args.get("date")
     if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
+        selected_date = datetime.datetime.fromisoformat(date_str)
     else:
-        selected_date = datetime.date.today()
+        selected_date = today_at_midnight()
+
+    activities_on_date = current_app.db.activity.find({"added": {"$lte": selected_date}})
+
+    completions = [
+        activity["activity"] for activity in current_app.db.completions.find({"date": selected_date})
+    ]
+
     return render_template(
         "index.html",
-        activities=activities,
+        activities=activities_on_date,
         selected_date=selected_date,
-        completions=completions[selected_date],
+        completions=completions,
         title="Activity Tracker - Home"
-    )
-
-
-@pages.route("/add", methods=["GET", "POST"])
-def add_activity():
-    if request.method == "POST":
-        activity = request.form.get("activity")
-        activities.append(activity)
-    return render_template(
-        "add_activity.html",
-        title="Activity Tracker - Add Activity",
-        selected_date=datetime.date.today()
     )
 
 
 @pages.route("/complete", methods=["POST"])
 def complete():
     date_string = request.form.get("date")
-    activity = request.form.get("activityName")
-    date = datetime.date.fromisoformat(date_string)
-    completions[date].append(activity)
+    date = datetime.datetime.fromisoformat(str(date_string))
+    activity = request.form.get("activityId")
+
+    # completions[date].append(activity)
+    current_app.db.completions.insert_one({"date": date, "activity": activity})
 
     return redirect(url_for("activities.index", date=date_string))
+
+
+@pages.route("/add", methods=["GET", "POST"])
+def add_activity():
+    today = today_at_midnight()
+
+    if request.form:
+        current_app.db.activity.insert_one(
+            {"_id": uuid.uuid4().hex, "added": today, "name": request.form.get("activity")}
+        )
+
+    # if request.method == "POST":
+    #     activity = request.form.get("activity")
+    #     activities.append(activity)
+    return render_template(
+        "add_activity.html",
+        title="Activity Tracker - Add Activity",
+        selected_date=today
+    )
